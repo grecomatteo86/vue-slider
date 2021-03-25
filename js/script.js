3b65116f7a4da9baf1dd30c64db7ed7b1a89e51b
Matteo <grecomatteo86@gmail.com>
2021-03-25 16:42:18 +0100

    image index setting

--- js/script.js
+++ js/script.js
@@ -2,7 +2,13 @@ var app = new Vue (
   {
     el:'#root',
     data:{
-      
+      images:[
+        'https://gdsit.cdn-immedia.net/2016/05/toni9.jpg',
+        'https://media.gettyimages.com/photos/palermo-italy-palermos-midfielder-eugenio-corini-jubilates-after-he-picture-id72512833?k=6&m=72512833&s=612x612&w=0&h=mf67qFxY_b15F9sizDdlHChEJh8o7JG9jNUNhp-OFOQ=',
+        'https://www.rosanerolive.it/wp-content/uploads/2020/05/barzagli-533x261.jpg',
+        'https://media.gettyimages.com/photos/salvo-ficarra-and-valentino-picone-pose-during-a-us-citta-di-palermo-picture-id464030392?k=6&m=464030392&s=612x612&w=0&h=dMXjivZNM24e0CWKuYIew5RMRmxX73nxD4097zUeFKU='
+      ],
+      imgIndex:0
     }
   }
 );
