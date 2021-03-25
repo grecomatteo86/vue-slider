0290e8ef2d6ee9bb6a774e63842d5ab6cdf1a61d
Matteo <grecomatteo86@gmail.com>
2021-03-25 18:00:05 +0100

    bullets setting

--- js/script.js
+++ js/script.js
@@ -7,6 +7,8 @@ var app = new Vue (
         'https://media.gettyimages.com/photos/palermo-italy-palermos-midfielder-eugenio-corini-jubilates-after-he-picture-id72512833?k=6&m=72512833&s=612x612&w=0&h=mf67qFxY_b15F9sizDdlHChEJh8o7JG9jNUNhp-OFOQ=',
         'https://www.rosanerolive.it/wp-content/uploads/2020/05/barzagli-533x261.jpg',
         'https://media.gettyimages.com/photos/franco-brienza-of-palermo-celebrates-after-scoring-his-teams-second-picture-id155010917?k=6&m=155010917&s=612x612&w=0&h=456KWsprF4caOq8-OVjr5IsAy7xhNE4nwZ4WF_g17rs=',
+        'https://media.gettyimages.com/photos/palermo-italy-palermos-christian-zaccardo-jubilates-after-he-scored-a-picture-id72411644?k=6&m=72411644&s=612x612&w=0&h=5z817t81Ce74X5iRbzzJ6L9-6KnSrqfa5r-hlwH6BsA=',
+        'https://media.gettyimages.com/photos/palermo-italy-palermos-fabio-simplicio-of-brazil-jubilates-after-he-picture-id72785934?k=6&m=72785934&s=612x612&w=0&h=FBnEnUo4CCmwtqxacg1tE2F6zPtAA2P5U5WWAJonU3U=',
         'https://media.gettyimages.com/photos/salvo-ficarra-and-valentino-picone-pose-during-a-us-citta-di-palermo-picture-id464030392?k=6&m=464030392&s=612x612&w=0&h=dMXjivZNM24e0CWKuYIew5RMRmxX73nxD4097zUeFKU='
       ],
       imgIndex:0
